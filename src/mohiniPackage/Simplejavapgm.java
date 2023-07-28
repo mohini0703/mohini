14be9f85ce34ee6a2e15780d2bb199dd1b40b7b2
Megha <Megha@DESKTOP-3R7FA6I>
2023-07-28 16:10:39 +0530

    hi all is committed

---
+++ src/mohiniPackage/Simplejavapgm.java
@@ -0,0 +1,14 @@
+package mohiniPackage;
+
+public class Simplejavapgm {
+	int a=10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hello java");
+    Simplejavapgm s=new Simplejavapgm();
+    System.out.println(s.a);
+    
+	}
+
+}
---
+++ src/mohiniPackage/StarPattern1.java
@@ -0,0 +1,16 @@
+package mohiniPackage;
+
+public class StarPattern1 {
+
+	public static void main(String[] args) {
+	   for(int i=0;i<4;i++)
+	   {
+		   for(int j=4;j<i+1;j++)
+		   {
+			   System.out.print("*");
+		   }
+	   }System.out.println("");
+
+	}
+
+}
